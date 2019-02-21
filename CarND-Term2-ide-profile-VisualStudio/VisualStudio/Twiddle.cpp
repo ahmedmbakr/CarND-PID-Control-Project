19b6dd7f7fd9e7465485470dbaf7c7c9b100d313
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-02-21 19:12:49 +0200

    - Add dummy implementation for functions in Twiddle class.

--- CarND-Term2-ide-profile-VisualStudio/VisualStudio/Twiddle.cpp
+++ CarND-Term2-ide-profile-VisualStudio/VisualStudio/Twiddle.cpp
@@ -1,14 +1,20 @@
 #include "Twiddle.h"
 
+double Twiddle::Try_params::run()
+{
+	return 0;
+}
+
 double Twiddle::optimize(double tol = 0.2)
 {
 	Try_params try_params(params[0], params[1], params[2]);
 	double best_err = try_params.run();
-	double sum_params = 0;
+	//double sum_params = 0;
 	while(sum_params() < tol)
 	{
 		
 	}
+	return 0;
 }
 
 double Twiddle::sum_params()
