cd2cb179a8221d12406a61dac72f2f1fad7be659
Ahmed Bakr <ahmed.m.ali.bakr@gmail.com>
2019-02-21 20:01:50 +0200

    - Add the I component value.

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ int main()
   PID pid;
   // TODO: Initialize the pid variable.
   //pid.Init(2.9331227688652457, 10.326589894591526, 0.49316041639454505);
-  pid.Init(0.3, 0, 10);
+  pid.Init(0.3, 0.001, 10);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER>* ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
